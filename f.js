f459d8b2a0c384d045a97d1851d888be19ed1e72
Temberland <Temberland@users.noreply.github.com>
2024-01-12 05:25:54 +1000

    Небольшой причес кода

--- f.js
+++ f.js
@@ -1,3 +1,28 @@
+//Сменить цвет текста в шапке
+function change_white(){
+  document.querySelector('.cap_text_navigation').style.color = 'white'; 
+  document.querySelector('.SM1').style.color = 'white'; 
+  document.querySelector('.SM2').style.color = 'white'; 
+  document.querySelector('.SM3').style.color = 'white'; 
+}
+function change_black(){
+  document.querySelector('.cap_text_navigation').style.color = 'black'; 
+  document.querySelector('.SM1').style.color = 'black'; 
+  document.querySelector('.SM2').style.color = 'black'; 
+  document.querySelector('.SM3').style.color = 'black'; 
+}
+//  (выдвинуть / вдвинуть)
+function hide(){
+  menu.style.transition = 'transform 0.3s ease-in-out';
+  menu.style.transform = 'translateX(350px)';
+  offOut.style.transform = 'translateX(-500px)'
+  isMenuOpen = false;
+}
+function show(){
+  menu.style.transition = 'transform 0.3s ease-in-out';
+  menu.style.transform = 'translateX(0)';
+  offOut.style.transform = 'translateX(0)';
+}
 
 
 document.addEventListener('DOMContentLoaded', function() {  
@@ -9,21 +34,14 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('.cap').style.backgroundColor = 'white';   
         document.querySelector('.cap').style.boxShadow = '0px 7px 7px 0px rgba(0, 0, 0, 0.1)';
         document.querySelector('.cap_text').style.color = 'black'; 
-        document.querySelector('.cap_text_navigation').style.color = 'black'; 
-        document.querySelector('.SM1').style.color = 'black'; 
-        document.querySelector('.SM2').style.color = 'black'; 
-        document.querySelector('.SM3').style.color = 'black'; 
+        change_black();
       } else {   
           document.querySelector('.cap').style.transition = 'background-color 0.7s ease';   
           document.querySelector('.cap').style.backgroundColor = '';   
           document.querySelector('.cap').style.boxShadow = 'none'; 
           document.querySelector('.cap_text').style.color = 'white';
         if (window.innerWidth > 800){
-           
-          document.querySelector('.cap_text_navigation').style.color = 'white'; 
-          document.querySelector('.SM1').style.color = 'white'; 
-          document.querySelector('.SM2').style.color = 'white'; 
-          document.querySelector('.SM3').style.color = 'white'; 
+          change_white();
         }
       }
     });  
@@ -37,28 +55,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
   burgerMenu.addEventListener('click', () => {
     if (isMenuOpen) {
-      menu.style.transition = 'transform 0.3s ease-in-out';
-      menu.style.transform = 'translateX(350px)';
-      offOut.style.transform = 'translateX(-500px)'
-      isMenuOpen = false;
+      hide();
     } else {
-      menu.style.transition = 'transform 0.3s ease-in-out';
-      menu.style.transform = 'translateX(0)';
-      offOut.style.transform = 'translateX(0)';
-      document.querySelector('.cap_text_navigation').style.color = 'black'; 
-      document.querySelector('.SM1').style.color = 'black'; 
-      document.querySelector('.SM2').style.color = 'black'; 
-      document.querySelector('.SM3').style.color = 'black'; 
+      show();
+      change_black();
       isMenuOpen = true;
     }
   });
 
   const offOut = document.querySelector('.off_out');    
   offOut.addEventListener('click', () => {
-      menu.style.transition = 'transform 0.3s ease-in-out';
-      menu.style.transform = 'translateX(350px)';
-      offOut.style.transform = 'translateX(-500px)'
-      isMenuOpen = false;
+    hide();
   });
 
   function performActionBasedOnScreenWidth() {  
@@ -73,22 +80,12 @@ document.addEventListener('DOMContentLoaded', function() {
       menu.style.transform = 'translateX(350px)';
       isMenuOpen = false
     }
-
-
-
     if ((screenWidth >= 802) && (pageYOffset === 0)) {  
-      document.querySelector('.cap_text_navigation').style.color = 'white';   
-      document.querySelector('.SM1').style.color = 'white';   
-      document.querySelector('.SM2').style.color = 'white';   
-      document.querySelector('.SM3').style.color = 'white'; 
+      change_white();
     }
     else{
-      document.querySelector('.cap_text_navigation').style.color = 'black'; 
-      document.querySelector('.SM1').style.color = 'black'; 
-      document.querySelector('.SM2').style.color = 'black'; 
-      document.querySelector('.SM3').style.color = 'black'; 
+      change_black();
     }   
-  }  
-      
+  }       
   // Вызов функции при изменении размера окна  
   window.addEventListener('resize', performActionBasedOnScreenWidth);
